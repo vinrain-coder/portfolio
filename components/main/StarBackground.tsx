4a7cd65574000b76ace68a7a99b1f28625563d3f
Vin Rain <vincentombogo57@gmail.com>
2024-09-18 13:07:10 +0300

    modify starbackground

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
+// @ts-ignore
 import * as random from "maath/random";
 
 const StarBackground = (props: any) => {
