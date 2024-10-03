29abd01f2ac4f97e9a67bf96a813ce79e7f611f1
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-10-03 20:11:08 +0300

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -17,7 +17,7 @@ const SkillText = () => {
       >
         <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
         <h1 className="Welcome-text text-[13px]">
-          Creating user-friendly interfaces with Next.js & MERN
+          Creating user-friendly interfaces
         </h1>
       </motion.div>
       <motion.div
